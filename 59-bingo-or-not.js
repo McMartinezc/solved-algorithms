c11e477c76306085b77b9eb7e15e3136391f539e
McMartinezc <psyence_mc@hotmail.com>
2023-10-23 20:05:07 +0200

    ok

--- 59-bingo-or-not.js
+++ 59-bingo-or-not.js
@@ -5,12 +5,28 @@
  * For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
 
 Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array. Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
+
+
+Cada número corresponde a una letra del alfabeto en orden (por ejemplo, 1 = A, 2 = B, etc.). Escribe una función en la que ganarás el juego si tus números pueden formar la palabra "BINGO". No es necesario que estén en el orden correcto en la lista de entrada. De lo contrario, perderás. Tus resultados deben ser "GANAR" o "PERDER", respectivamente.
  */
 
 function bingo(numbers) {
   // your winning code here
+  const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  const targetWord = ["B","I","N","G","O"];
+
+  //Convertimos el array de numeros a las letras que le corresponda
+  const numbersLetters = numbers.map(num=> alphabet[num-1]); //num- porque empieza por A-->1
+  console.log(numbersLetters)
+  
+  //Filtramos las letras de targerWord que esten presentes en la array proporcionada por el usuario
+  const bingoLetters = targetWord.filter(letter => numbersLetters.includes(letter));
+
+  console.log(bingoLetters)
+  return bingoLetters.length===5 ? "WIN" : "LOSE";
 }
 
 console.log(bingo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // "LOSE"
 console.log(bingo([21, 13, 2, 7, 5, 14, 7, 15, 9, 10])); // "WIN"
 console.log(bingo([21, 13, 1, 7, 5, 14, 7, 15, 9, 10])); // "WIN"
+console.log(bingo([21, 13, 2, 7, 5, 14, 7, 15, 9, 10,3])); // "WIN"
--- 60-will-you-survive-to-zombies.js
+++ 60-will-you-survive-to-zombies.js
@@ -3,7 +3,17 @@
  * 
  * Enunciado: https://www.codewars.com/kata/5deeb1cc0d5bc9000f70aa74 Kyu 7
 
-    
+    "Te encuentras en una situación bastante desafortunada. Has herido tu pierna y no puedes caminar, y un grupo de zombies se acerca lentamente hacia ti, decididos a comerse tu cerebro. Afortunadamente, eres un tirador experto y tienes tu confiable rifle a tu alcance.
+
+Los zombies comienzan a una distancia de X metros y se mueven a una velocidad de 0.5 metros por segundo. Cada segundo, primero disparas a un zombie y luego los zombies restantes avanzan otros 0.5 metros.
+
+Si algún zombie logra llegar a 0 metros, serás comido. Si te quedas sin balas antes de disparar a todos los zombies, también serás comido. Para simplificar, podemos ignorar el tiempo dedicado a recargar.
+
+Escribe una función que acepte el número total de zombies, una distancia en metros y la cantidad de balas que tienes.
+
+Si disparas a todos los zombies, devuelve "Disparaste a todos los X zombies". Si te comen antes de matar a todos los zombies y antes de quedarte sin balas, devuelve "Disparaste a X zombies antes de ser comido: abrumado". Si te quedas sin balas antes de disparar a todos los zombies, devuelve "Disparaste a X zombies antes de ser comido: te quedaste sin balas".
+
+(Si te quedas sin balas al mismo tiempo que los zombies restantes llegan a ti, devuelve "Disparaste a X zombies antes de ser comido: abrumado"). ¡Buena suerte! (Creo que la vas a necesitar)."
 */
 
 function zombieShootout(zombies, range, ammo) {
